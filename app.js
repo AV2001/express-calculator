b78d14bef113532ab8e60e01f9628bd22c1f278a
Anirudh Vadlamani <anirudhvadlamani2001@gmail.com>
2023-10-30 11:02:13 +0400

    implement functionality for median route

--- app.js
+++ app.js
@@ -35,6 +35,41 @@ app.get('/mean', (req, res, next) => {
     }
 });
 
+// Calculate Median
+app.get('/median', (req, res, next) => {
+    try {
+        let { nums } = req.query;
+        if (!nums) throw new ExpressError('nums are required.', 400);
+        nums = nums.split(',');
+
+        const parsedNums = nums.map((num) => parseInt(num));
+
+        // Find the element that is NaN
+        const invalidInput = nums.filter((num) => isNaN(parseInt(num)));
+
+        if (invalidInput.length > 0) {
+            throw new ExpressError(`${invalidInput[0]} is not a number.`, 400);
+        }
+
+        // If length of nums is odd, get the middle number
+        if (parsedNums.length % 2 == 1) {
+            const median = parsedNums[Math.floor(parsedNums.length / 2)];
+            return res
+                .status(200)
+                .json({ response: { operation: 'median', value: median } });
+        } else {
+            const length = parsedNums.length;
+            const median =
+                (parsedNums[length / 2 - 1] + parsedNums[length / 2]) / 2;
+            return res
+                .status(200)
+                .json({ error: { operation: 'median', value: median } });
+        }
+    } catch (err) {
+        next(err);
+    }
+});
+
 // Error Handler Route
 app.use((err, req, res, next) => {
     const message = err.message;
